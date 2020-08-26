557fee85db4ef8b14d3bbcda4652362c28d50d87
André Rios <andre.rios.freitas@gmail.com>
2020-08-26 17:37:31 +0100

    desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+    const income = this.transactions
+      .map(transaction => {
+        if (transaction.type === 'income') {
+          return transaction.value;
+        }
+        return 0;
+      })
+      .reduce(reducer);
+
+    const outcome = this.transactions
+      .map(transaction => {
+        if (transaction.type === 'outcome') {
+          return transaction.value;
+        }
+        return 0;
+      })
+      .reduce(reducer);
+
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
